3f3662450691b2e91d26e828e4aeeca227360abf
Amir <amirkhorasani2007@gmail.com>
2026-06-09 23:27:44 +0330

    feat: add JewelryCard component and implement Latest products display

---
+++ src/components/modules/jewelrycard/JewelryCard.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import { GoHeart } from "react-icons/go";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+
+export interface JewelryItem {
+  id: string | number;
+  title: string;
+  price: number;
+  weight: number;
+  img: string;
+  type: string;
+}
+
+const JewelryCard = ({ id, title, price, weight, img, type }: JewelryItem) => {
+  return (
+    <div className="relative bg-white p-3.5 md:p-5 rounded-3xl flex flex-col justify-center transition-all cursor-pointer hover:scale-101 hover:shadow-lg">
+      
+      <div className="flex justify-between items-center">
+        <p className="text-xs md:text-sm text-neutral-400">#{type}</p>
+
+        <div className="flex gap-2.5 md:gap-3 *:hover:text-[#10494b] *:duration-500 text-lg md:text-[22px] text-neutral-400">
+          <GoHeart />
+          <Link href={`/jewelry/${id}`}>
+            <button title="افزودن به سبد خرید">
+              <HiOutlineShoppingBag />
+            </button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="relative w-full md:my-1.5 flex items-center justify-center overflow-hidden">
+        <img
+          src={img}
+          alt={title}
+          className="rounded-2xl max-w-55 md:max-w-73 object-contain"
+        />
+      </div>
+
+      <div>
+        <h1 className="text-sm font-yekan md:text-base font-semibold text-neutral-800">{title}</h1>
+        <div className="flex items-center gap-1 text-[10px] md:text-[13px] mt-1 md:mt-0.5 font-medium text-neutral-500">
+          <p>وزن :</p>
+          {new Intl.NumberFormat("fa-IR").format(weight)}
+        </div>
+      </div>
+
+      <div className="flex justify-end items-center font-yekan">
+        <div className="text-[14.5px] md:text-base text-black flex flex-col gap-0.5 items-end font-semibold">
+          <span className="text-[10px] md:text-xs font-medium text-neutral-500">تومان</span>
+          {new Intl.NumberFormat("fa-IR").format(price)}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JewelryCard;
---
+++ src/components/templates/index/latest/Latest.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import JewelryCard, { JewelryItem } from "../../../modules/jewelrycard/JewelryCard";
+import db from "../../../../data/db.json"
+import Link from "next/link"
+import { HiArrowLeft } from "react-icons/hi";
+
+interface LatestProductsProps {
+  products: JewelryItem[];
+}
+
+const Latest = ({ products }: LatestProductsProps) => {
+  const latest = db.products.slice(-8).reverse();
+
+  return (
+    <div className="w-full py-6">
+      <div className="flex items-center justify-between gap-7 w-full mb-6 md:mb-12 px-4.5 md:px-17">
+        <h2 className="text-xl md:text-2xl font-bold text-black font-yekan whitespace-nowrap [word-spacing:-2px]">
+          جدیدترین <span className="text-[#10494b]">جواهرات</span>
+        </h2>
+
+        <div className="hidden sm:block w-16 sm:w-full h-[1px] bg-neutral-300/70" />
+
+        <Link href="/" className="flex items-center font-yekan text-xs md:text-sm whitespace-nowrap gap-2 font-semibold text-neutral-600">
+          همه جواهرات <HiArrowLeft size={13} />
+        </Link>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3.5 md:gap-5 px-4.5 md:px-17">
+        {latest.map((item) => (
+          <JewelryCard key={item.id} {...item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Latest;
