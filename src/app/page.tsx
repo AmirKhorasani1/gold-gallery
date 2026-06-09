6e08adab2fda850883d320f300f0b2ae93e9122b
Amir <amirkhorasani2007@gmail.com>
2026-06-09 23:27:13 +0330

    refactor: update import paths and remove unused icon components from Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,16 @@
-import Footer from "../components/modules/footer/Footer";
-import Navbar from "../components/modules/navbar/Navbar";
-import Banner from "../components/templates/index/banner/Banner";
-import Categories from "../components/templates/index/categories/Categories";
-import CustomerReviews from "../components/templates/index/customer-reviews/CustomerReviews";
-import CollectionIntro from "../components/templates/index/iran-collection/CollectionIntro";
-import CollectionProducts from "../components/templates/index/iran-collection/CollectionProducts";
-import Latest from "../components/templates/index/latest/Latest";
+import Footer from "@/components/modules/footer/Footer";
+import Navbar from "@/components/modules/navbar/Navbar";
+import Banner from "@/components/templates/index/banner/Banner";
+import Categories from "@/components/templates/index/categories/Categories";
+import CustomerReviews from "@/components/templates/index/customer-reviews/CustomerReviews";
+import CollectionIntro from "@/components/templates/index/iran-collection/CollectionIntro";
+import CollectionProducts from "@/components/templates/index/iran-collection/CollectionProducts";
+import Latest from "@/components/templates/index/latest/Latest";
 //import Magazine from "../components/templates/index/magazine/Magazine";
-
-import {
-  GiBigDiamondRing,
-  GiCrystalBall,
-  GiCutDiamond,
-  GiDiamondHard,
-  GiDiamondHilt,
-  GiDiamonds,
-  GiEmerald,
-  GiGemChain,
-  GiGemNecklace,
-  GiGems,
-  GiPearlNecklace,
-  GiTopaz,
-} from "react-icons/gi";
 
 const Home = () => {
   return (
-    <div className="flex flex-col gap-12 md:gap-25">
+    <div className="flex flex-col gap-17 md:gap-25">
       <Navbar />
       <Banner />
       <Latest />
@@ -33,22 +18,6 @@ const Home = () => {
       <CollectionIntro />
       <CollectionProducts />
       <CustomerReviews />
-
-      <div className="flex flex-wrap items-center justify-center gap-6 text-black text-4xl">
-        <GiBigDiamondRing />
-        <GiCrystalBall />
-        <GiCutDiamond />
-        <GiDiamondHard />
-        <GiDiamondHilt />
-        <GiDiamonds />
-        <GiEmerald />
-        <GiGemChain />
-        <GiGemNecklace />
-        <GiGems />
-        <GiPearlNecklace />
-        <GiTopaz />
-      </div>
-
       <Footer />
     </div>
   );
