b45ccff386852d0796ea478d7649553a0cf3d509
Amir <amirkhorasani2007@gmail.com>
2026-06-11 23:57:55 +0330

    feat(user): create user schema and model

---
+++ models/User.ts
@@ -0,0 +1,61 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export type UserRole = "USER" | "ADMIN";
+
+export interface IUser extends Document {
+  name: string;
+  email?: string;
+  phone: string;
+  password?: string;
+  role: UserRole;
+  refreshToken?: string;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      unique: true,
+      sparse: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+
+    refreshToken: {
+      type: String,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
