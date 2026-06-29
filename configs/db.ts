22a574e30b9f3d365657ee7dc9d152a74b5e273f
Amir <amirkhorasani2007@gmail.com>
2026-06-30 00:33:23 +0330

    refactor(db): reorganize import statements for clarity

--- configs/db.ts
+++ configs/db.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+import "@/models/User";
+import "@/models/Product";
+import "@/models/Wishlist";
+import "@/models/Comment";
+
 const connectToDB = async () => {
   try {
     if (mongoose.connections[0].readyState) {
@@ -15,7 +20,6 @@ const connectToDB = async () => {
     await mongoose.connect(mongoUrl);
 
     console.log("Connected to DB successfully");
-
   } catch (err) {
     console.log("DB Connection Error:", err);
   }
